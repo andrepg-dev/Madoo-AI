e3976881933ec0755175c34a179c1c8a6b687bfa
André Ponce <asponceg@gmail.com>
2026-06-19 11:10:12 -0600

    fix client template previews

--- apps/client/components/projects/ProjectLibrary.tsx
+++ apps/client/components/projects/ProjectLibrary.tsx
@@ -188,7 +188,7 @@ function ProjectPreview({
       {previewUrl ? (
         <img
           alt=""
-          className="h-full w-full object-cover object-top"
+          className="h-full w-full object-contain object-top"
           loading="lazy"
           src={previewUrl}
         />
